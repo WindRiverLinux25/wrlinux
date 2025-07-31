2747141fe04a50ad9da8cfdb431335e39512c95f
Robert Yang <liezhi.yang@windriver.com>
2025-07-31 04:19:05 -0700

    Remove chromium related configs
    Issue: LINUXEXEC-36075
    
    The chromium has been dropped from LTS25.
    
    Signed-off-by: Robert Yang <liezhi.yang@windriver.com>

--- recipes-graphics/wr-kiosk/files/wr-chromium-web-kiosk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# setting default display for chromium
-export DISPLAY=:0
-
-chromium @KIOSK-MODE-FLAG@ \
-    --load-extension="@EXTENSIONS@" \
-    --window-size=@WINDOW-SIZE@ \
-    @STARTING-URL@
